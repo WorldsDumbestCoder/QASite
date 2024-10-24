3a280eefbebf07bd94cf7ef2d1905b5d8da4074f
MyBrain=NULL (Ethan James John Mullally) <124309800+WorldsDumbestCoder@users.noreply.github.com>
2024-10-24 09:29:27 -0400

    Create Ask.php

---
+++ Ask.php
@@ -0,0 +1,15 @@
+<?php
+  if($_REQUEST["REQUEST_METHOD"] == "POST"){
+    if(isset($_POST["QUESTION_BODY"]) && isset($_POST["QUESTION_TITLE"])){
+      $question_body = htmlspecialchars_encode($_POST["QUESTION"]);
+      $question_title = htmlspecialchars_encode($_POST["QUESTION_TITLE"]);
+      $url = array(
+        "title" => $question_title,
+        "username" => null,
+      );
+      $newURL = http_build_query("upload.php?" . $url);
+    }
+  }else{
+    die("Request Method Not Allowed");
+  }
+?>
