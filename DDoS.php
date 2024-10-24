d5b72227a4e8c869d7e2a2ff3d0354cabf68d5a4
MyBrain=NULL (Ethan James John Mullally) <124309800+WorldsDumbestCoder@users.noreply.github.com>
2024-10-24 09:13:39 -0400

    Update DDoS.php

--- DDoS.php
+++ DDoS.php
@@ -5,17 +5,18 @@ if(!isset($_SESSION["MAX_REQUEST"])){
   $_SESSION["REQUESTS_SENT"] = 0;
   $_SESSION["MAX_TIME"] = 50;
 }
-if(!isset($_COOKIE["BLOCK_REQUEST"]) && time() - $_SESSION["MAX_TIME"] <= 500]){
+if(!isset($_COOKIE["BLOCK_REQUEST"]) && time() - $_SESSION["MAX_TIME"] <= 500){
    if($_SESSION["REQUESTS_SENT"] <= $_SESSION["MAX_REQUEST"]){
        $_SESSION["REQUEST_SENT"]++;
    }else{
      http_response_code(403);
      header("HTTP/1.0 403 Forbidden");
-     $expires = time() * 500;
+     $expires = time() * 2;
      setcookie("BLOCK_REQUEST", true, $expires);
   }
 }else{
   http_response_code(403);
   header("HTTP/1.0 403 Forbidden");
+  die("Sorry, but you can not access this website for a while");
 }
 ?>
